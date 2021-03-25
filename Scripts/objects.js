95d7c58fc1d557596a2adc5e4553c003692d5ad0
GJesus <gabriel.ds.jesus@gmail.com>
2021-03-25 18:44:54 -0300

    canvas automatico

---
+++ Scripts/objects.js
@@ -0,0 +1,114 @@
+let q1 = {
+    ads:0,
+    gpi:0,
+    grh:0,
+    dsm:0
+}
+
+let q2 = {
+    m:0,
+    n:0
+}
+
+let q4 = {
+    a:0
+}//gerar variaveis automaticamente
+
+let q5 = {
+    a:0
+}//gerar variaveis automaticamente
+
+let q6 = {
+    a:0
+}//gerar variaveis automaticamente
+
+//let q7 = {} esquema diferente
+
+let q8 = {
+    s:0,
+    c:0,
+    s:0,
+    v:0
+}
+
+let q9 = {
+    n:0,
+    v:0,
+    f:0,
+    aud:0,
+    aut:0,
+    fal:0
+}
+
+let q10 = {
+    aut:0,
+    s:0,
+    d:0,
+    aud:0,
+    v:0,
+    fal:0,
+    f:0,
+    n:0
+}
+
+let q11 = {
+    n:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mQuat:0
+}
+
+let q12 = {
+    pais:0,
+    eof:0,
+    amg:0,
+    s:0
+}
+
+//let q13 = {} esquema diferente igual a q7
+
+let q14 = {
+    p:0,
+    a:0,
+    c:0,
+    f:0,
+    ar:0,
+    men:0
+}
+
+//let q15 = {} esquema diferente igual a q7
+let q16 = {}
+let q17 = {}
+let q18 = {}
+let q19 = {}
+let q20 = {}
+let q21 = {}
+let q22 = {}
+let q23 = {}
+let q24 = {}
+let q25 = {}
+let q26 = {}
+let q27 = {}
+let q28 = {}
+let q29 = {}
+let q30 = {}
+let q31 = {}
+let q32 = {}
+let q33 = {}
+let q34 = {}
+let q35 = {}
+let q36 = {}
+let q37 = {}
+let q38 = {}
+let q39 = {}
+let q40 = {}
+let q41 = {}
+let q42 = {}
+
+
+
+
+
+
