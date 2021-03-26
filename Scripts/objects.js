30264c372a94d1643d96d2c7a9fd8bbaa252a68e
GJesus <gabriel.ds.jesus@gmail.com>
2021-03-26 13:57:15 -0300

    Aumentando os graficos e melhorando o código

--- Scripts/objects.js
+++ Scripts/objects.js
@@ -1,114 +1,250 @@
-let q1 = {
-    ads:0,
-    gpi:0,
-    grh:0,
-    dsm:0
+//variaveis criadas da seguinte forma q = questão n= número da questão, exemplo : q6 = questão 6
+let q6 ={//genero
+    f:0,
+    m:0,
+    ns:0
 }
 
-let q2 = {
+let q4 ={//estado do brasil
+    s:0,
+    m:0,
+    p:0,
+    pe:0
+}
+
+let q1 ={//qual o curso
+    a:0,
+    g:0,
+    r:0,
+    d:0
+}
+
+let q2 ={//periodo
     m:0,
     n:0
 }
 
-let q4 = {
-    a:0
-}//gerar variaveis automaticamente
+let q5 ={//cidade
+    f:0,
+    i:0,
+    p:0,
+    rc:0,
+    rp:0,
+    s:0
+}
 
-let q5 = {
-    a:0
-}//gerar variaveis automaticamente
+let q7 ={//idade
+    "18e20":0,
+    "21e25":0,
+    "26e30":0,
+    "31e50":0,
+    "m50":0,
+    
+}
 
-let q6 = {
-    a:0
-}//gerar variaveis automaticamente
-
-//let q7 = {} esquema diferente
-
-let q8 = {
+let q8 ={//estado civil
     s:0,
     c:0,
-    s:0,
+    o:0
+}
+
+let q9 ={//Possir deficiencia
+    n:0,
     v:0
 }
 
-let q9 = {
-    n:0,
-    v:0,
-    f:0,
-    aud:0,
-    aut:0,
-    fal:0
-}
-
-let q10 = {
-    aut:0,
+let q10 ={//Conviver Deficiencia
+    ad:0,
+    at:0,
     s:0,
-    d:0,
-    aud:0,
-    v:0,
-    fal:0,
-    f:0,
     n:0
+
 }
 
-let q11 = {
+let q11 ={//Quantidade de filhos
     n:0,
+    u:0,
+    d:0,
+    t:0
+}
+
+let q12 ={//Com quem voce vive
+    a:0,
+    e:0,
+    p:0,
+    s:0
+}
+
+let q13 ={//Quantas pessoas na sua casa
+    a:0,
+    b:0,
+    c:0,
+    d:0,
+    e:0,
+    f:0,
+    g:0
+}
+
+let q14 ={
+    p:0, // proprio
+    a:0, //alugado
+    c:0, //cedido
+    f:0, //financiado
+    ar:0, //arrendado
+    men:0, //mensalista
+}
+
+let q15 = {
+    "1e5":0,
+    "6e10":0,
+    "11e15":0,
+    "16e20":0,
+    "m20":0
+}
+
+let q16 ={
+    "a2":0, //até dois
+    "m2a5":0,   //mais de dois
+    "m5a10":0,  //mais de cinco
+    "m10a20":0, //mais de 10
+    "m20":0,    //mais de 20
+    "pf":0,     //prefiro não dizer
+}
+
+
+//questão 17 dividida em 10 perguntas// de a até j
+let q17a = {
+    zero:0,
     um:0,
     dois:0,
     tres:0,
     quat:0,
-    mQuat:0
+    mquat:0
 }
 
-let q12 = {
-    pais:0,
-    eof:0,
-    amg:0,
-    s:0
+let q17b = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
 }
 
-//let q13 = {} esquema diferente igual a q7
-
-let q14 = {
-    p:0,
-    a:0,
-    c:0,
-    f:0,
-    ar:0,
-    men:0
+let q17c = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
 }
 
-//let q15 = {} esquema diferente igual a q7
-let q16 = {}
-let q17 = {}
-let q18 = {}
-let q19 = {}
-let q20 = {}
-let q21 = {}
-let q22 = {}
-let q23 = {}
-let q24 = {}
-let q25 = {}
-let q26 = {}
-let q27 = {}
-let q28 = {}
-let q29 = {}
-let q30 = {}
-let q31 = {}
-let q32 = {}
-let q33 = {}
-let q34 = {}
-let q35 = {}
-let q36 = {}
-let q37 = {}
-let q38 = {}
-let q39 = {}
-let q40 = {}
-let q41 = {}
-let q42 = {}
+let q17d = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17e = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17f = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17g = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17h = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17i = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
 
+let q17j = {
+    zero:0,
+    um:0,
+    dois:0,
+    tres:0,
+    quat:0,
+    mquat:0
+}
+// fim da questão 17
+
+let q18 = {
+    tel:0, //telefone fixo
+    inte:0, //Internet
+    tv:0, //tv por assinatura
+    emp:0, //empregada
+    na:0 //nenhum
+}
+
+//questão 19 tem 5 perguntas
+let q19a = {
+    s:0,
+    n:0
+}
+
+let q19b = {
+    nt:0, //nao trabalho
+    sri:0, //sou registrado em industria
+    src:0, //sou registrado em comercio
+    sre:0, //sou registrado em empresa
+    sep:0, //sou registrado publico
+    sat:0, //sou autonomo
+    semp:0, // sou empresario
+    sest:0 //sou estagiario
+}
+
+let q19c = {
+    nt:0, //nao trabalho
+    tac:0, //trabnalho na area do curso
+    tfc:0, //trabalho fora da area do curso
+}
+
+let q19d = {
+    nt:0, //nao trabalho
+    m:0, // manha
+    t:0, //tarde
+    n:0, //noite
+    mt:0, // manha tarde
+    mn:0, //manha noite
+    tn:0, //tarde noite
+    regt:0 //regime de turnos
+}
+
+//ultima questão irrelevante
+//fim da questão 19
