45cf6cb140af59d88c4e532ed91711f7b9071d2d
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2021-04-07 20:08:45 -0300

    Adicionando novo formulario e criando funcoes

--- Scripts/login.js
+++ Scripts/login.js
@@ -9,3 +9,40 @@ buttonLog.addEventListener("click",function(){
 buttonSign.addEventListener("click",function(){
     body.className = "signUpJs"
 })
+
+
+function fazPost(url, body){
+    console.log("Body=", body)
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(body)
+
+    request.onload = function(){
+        console.log(this.responseText)
+    }
+
+    return request.responseText
+}
+
+function cadastrarUser(){
+    event.preventDefault()
+    let url = "localhost/users/cadastros"
+    let nome = document.getElementById('name').value 
+    let email = document.getElementById('email').value
+    let pass = document.getElementById('pass').value
+    console.log(nome)
+    console.log(email)
+    console.log(pass)
+
+    let body = {
+            "user": email,
+            "senha": pass,
+            "name": nome
+    }
+
+    fazPost(url, body)
+
+}
+
+
