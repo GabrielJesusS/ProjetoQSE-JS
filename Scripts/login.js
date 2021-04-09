7c95f743ef221c32ae7adcd3725b56a8ae2a7b03
Paulo Ricardo Acílio Chimelo <prac0402@gmail.com>
2021-04-08 22:12:21 -0300

    Criando Api de Login e iniciando a do formulario

--- Scripts/login.js
+++ Scripts/login.js
@@ -13,11 +13,15 @@ buttonSign.addEventListener("click",function(){
 
 function fazPost(url, body){
     console.log("Body=", body)
-    let request = new XMLHttpRequest()
+    let request = new XMLHttpRequest(); 
+    console.log('a')
     request.open("POST", url, true)
+    console.log('b')
     request.setRequestHeader("Content-type", "application/json")
+    console.log(body)
     request.send(body)
-
+    console.log('d')
+    console.log(url)
     request.onload = function(){
         console.log(this.responseText)
     }
@@ -27,22 +31,70 @@ function fazPost(url, body){
 
 function cadastrarUser(){
     event.preventDefault()
-    let url = "localhost/users/cadastros"
-    let nome = document.getElementById('name').value 
+    let url = "http://localhost:3000/users/cadastros"
+    let nome = document.getElementById('name').value
     let email = document.getElementById('email').value
     let pass = document.getElementById('pass').value
     console.log(nome)
     console.log(email)
     console.log(pass)
 
-    let body = {
+    let body2 = {
             "user": email,
             "senha": pass,
             "name": nome
     }
 
-    fazPost(url, body)
+    //fazPost(url, body)
 
+    fetch(url,{
+        method: "POST",
+        body:JSON.stringify({
+            user:email,
+            senha:pass,
+            name:nome
+        }),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+    })
+
+}
+
+function fazGet(url, body){
+    console.log("Body=", body)
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true)   
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(body)
+
+    request.onload = function(){
+        console.log(this.responseText)
+    }
+
+    return request.responseText 
+}
+
+function logar(){
+    event.preventDefault()
+    let url = "http://localhost:3000/users/login"
+    let user = document.getElementById('user').value
+    let pass = document.getElementById('pass').value
+    console.log(nome)
+    console.log(pass)
+
+    let body ={
+        "user": user,
+        "senha": pass
+    }
+
+    fazGet(url, body)
 }
 
 
